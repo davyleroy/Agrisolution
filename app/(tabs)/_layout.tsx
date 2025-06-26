93efa71d9276f3bdc1fe677356e9d1ee7875606b
Davy Mbuto NKURUNZIZA <122352504+davyleroy@users.noreply.github.com>
2025-06-26 06:57:17 +0200

    Added useFrameworkReady.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import { Platform } from 'react-native';
-import { Home, Camera, History, BookOpen, Settings } from 'lucide-react-native';
+import { Chrome as Home, Camera, History, BookOpen, Settings } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
